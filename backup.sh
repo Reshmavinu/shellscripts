0d4a8d84a901522bb70ba523a14c642584b09d0d
Reshma Anna Babu <158140771+Reshmavinu@users.noreply.github.com>
2025-09-21 15:00:45 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -4,10 +4,10 @@ bckup_dir="/var/log/backup"
 DATE=$(date +"%Y-%m-%d_%H-%M-%S")
 bckupname="backup_app$DATE.tar.gz"
 mkdir -p "$bckup_dir"
-tar -cvf "$bckup_dir/$bckupname" -C "$src_dir" .
+tar -cvzf "$bckup_dir/$bckupname" -C "$src_dir" .
 if [ $? -eq 0 ]; then
  echo " backup suceeded"
 else
  echo "backup failed"
 fi
-find "/var/log/backup" -type f -name "*.tar.gz" -mtime +1  -exec rm -f {} \;
+find "$bckup_dir" -type f -name "*.tar.gz" -mtime +1  -exec rm -f {} \;
